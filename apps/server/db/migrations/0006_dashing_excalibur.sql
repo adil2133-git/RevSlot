4058671ddcc47f0fdc225d6d0a46f682ad77477e
adil2133-git <adilaadi340@gmail.com>
2026-08-26 15:56:52 +0530

    chore(db): reset and regenerate drizzle migrations

--- apps/server/db/migrations/0006_dashing_excalibur.sql
+++
@@ -1,2 +0,0 @@
-ALTER TYPE "public"."otp_purpose" ADD VALUE 'forgot_password';--> statement-breakpoint
-ALTER TYPE "public"."otp_purpose" ADD VALUE 'email_verification';
--- apps/server/db/migrations/0012_talented_weapon_omega.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "bookings" ADD COLUMN "cancelled_at" timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "bookings" ADD COLUMN "cancelled_reason" varchar(255);
